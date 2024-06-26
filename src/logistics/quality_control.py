a2443ac1d9c690b93a3bbc4613ddf0a9874cfc8f
165274183+jalapih@users.noreply.github.com <165274183+jalapih@users.noreply.github.com>
2024-06-26 10:14:37 +0300

    Locked platform and belt direct controls, renamed buttons and switch, moved rangefinder settings to on_reset, changed toggle switch function to event handler

--- src/logistics/quality_control.py
+++ src/logistics/quality_control.py
@@ -28,21 +28,21 @@ data = DataModel()
 ### CONSTRUCTION CODE - Add all code to setup the level (select map, spawn entities) here ###
 editor.select_map(SpawnableMaps.SmallWarehouse)
 editor.spawn_entity(SpawnableEntities.ConveyorBelt, "belt1", location=(-6.55, -4.0, 0.0), scale=(1.0, 1.0, 1.206), is_controllable=False)
-editor.spawn_entity(SpawnableEntities.ConveyorBelt, "belt2", location=(3.600,-4.000,0.000), scale=(1.000,1.000,0.842)) #generated-code
+editor.spawn_entity(SpawnableEntities.ConveyorBelt, "belt2", location=(3.600,-4.000,0.000), scale=(1.000,1.000,0.842), is_controllable=False) #generated-code
 
 editor.spawn_entity(SpawnableEntities.TurnableConveyorBelt, "turnable_belt1", location=(-1.45, -4.0, 0.0), is_controllable=False)
 
-editor.spawn_entity(SpawnableEntities.PushButton, "button1", location=(-4.25, -2.1, 2.45), rotation=(90.0, 0.0, 0.0))
-editor.spawn_entity(SpawnableEntities.PushButton, "button2", location=(-4.25, -2.1, 1.3), rotation=(90.0, 0.0, 0.0))
+editor.spawn_entity(SpawnableEntities.PushButton, "turn_button", location=(-4.25, -2.1, 2.45), rotation=(90.0, 0.0, 0.0))
+editor.spawn_entity(SpawnableEntities.PushButton, "discard_button", location=(-4.25, -2.1, 1.3), rotation=(90.0, 0.0, 0.0))
 
 editor.spawn_static_mesh(SpawnableMeshes.Cube, location = (-4.25, -2.25, 1.55), scale = (1.138, -0.218, 3.245), material = SpawnableMaterials.SimpleColor, color = ((0.25)/5,.2,.2), is_temp=False)
 
-editor.spawn_entity(SpawnableEntities.ToggleSwitch, unique_name="toggle1", location=(-4.15, -2.1, 1.95), rotation=(-90.0, 0.0, 180.0))
+editor.spawn_entity(SpawnableEntities.ToggleSwitch, unique_name="belt_brake", location=(-4.15, -2.1, 1.95), rotation=(-90.0, 0.0, 180.0))
 editor.spawn_entity(SpawnableEntities.ObjectSpawner, unique_name="spawner1", location=(-8.45, -3.95, 3.7), rotation=(0.0, 0.0, -90.0), is_controllable=False, is_readable=False)
 editor.spawn_entity(SpawnableEntities.RangeFinder, "range_finder1", location=(-4.25,-6.000,0.000), rotation=(0.000,0.000,180.000), scale=(1.000,1.000,1.876)) 
 editor.spawn_static_mesh(SpawnableMeshes.PalletBoxOpen, location=(-1.45, 1.0, 0.0))
 editor.spawn_entity(SpawnableEntities.TriggerZone, "discard_zone", location=(-1.450,1.050,1.400), scale=(1.395,1.107,0.163)) #generated-code
-editor.spawn_entity(SpawnableEntities.MovablePlatform, "platform1", location=(-1.40,-1.20,0.5)) 
+editor.spawn_entity(SpawnableEntities.MovablePlatform, "platform1", location=(-1.40,-1.20,0.5), is_controllable=False) 
 editor.spawn_entity(SpawnableEntities.DeliveryContainer, "container1", location=(7.250,-4.000,0.000)) #generated-code
 
 def spawn_next():
@@ -84,6 +84,15 @@ editor.specify_goal("deliver_products", f"Products delivered: {data.correct}/10"
 
 ### ON BEGIN PLAY CODE - Add any code that should be executed after constructing the level once. ###
 
+def on_toggle_handler(sender,simtime,is_on): 
+    if is_on:
+        TurnableConveyorBelt.first().set_target_speed(0)  # Stop when ON
+        print("Conveyor belt brake switched to ON")
+    else:
+        TurnableConveyorBelt.first().set_target_speed(4)  # Move when OFF
+        print("Conveyor belt brake switched to OFF")
+    
+
 def on_discard(trig:TriggerZone, gt:float, e:TriggerEvent):
     
     data.box_count -= 1
@@ -114,12 +123,11 @@ def on_delivered(trig:DeliveryContainer, gt:float, e:TriggerEvent):
     
     
 def begin_play():
-    print("begin play")
-    range_finder = RangeFinder.first()
-    range_finder.editor_set_can_read_rfid_tags(True)
-    range_finder.editor_set_max_range(300)
+    print("begin play") 
     trigg = TriggerZone.first()
     trigg.on_triggered(on_discard)
+    toggle = ToggleSwitch.first()
+    toggle.on_toggle(on_toggle_handler)
     delivery_box = DeliveryContainer.first()
     delivery_box.on_delivered(on_delivered)
     on_reset()
@@ -132,13 +140,15 @@ editor.on_begin_play(begin_play)
 ### ON LEVEL RESET CODE - Add code that should be executed on every level reset. ###
 def on_reset():
     print("Level resetting")
-    random.seed()
+    range_finder = RangeFinder.first()
+    range_finder.editor_set_max_range(300)
+    range_finder.editor_set_can_read_rfid_tags(True)
     ConveyorBelt.find("belt1").set_target_speed(5)
     ConveyorBelt.find("belt2").set_target_speed(5)
-    if ToggleSwitch.first().get_is_switched_on(): ToggleSwitch.first().toggle()
     data.reset()
     editor.set_goal_state("deliver_products", GoalState.Open)
-    
+    if ToggleSwitch.first().get_is_switched_on() == False:
+        TurnableConveyorBelt.first().set_target_speed(4)
 
 editor.on_level_reset(on_reset)
 
@@ -148,7 +158,7 @@ editor.on_level_reset(on_reset)
 ### ON PLAYER COMMAND CODE - Add code that should be executed each time the player issues a code command to an entity
 def on_player_command(gametime:float, entity_type:str, entity_name:str, command:str, val:NPArray):
     
-    if entity_name == "button1" and command == "Press":
+    if entity_name == "turn_button" and command == "Press":
         if data.current_rotation < 80:
             TurnableConveyorBelt.first().set_target_rotation(90)
             print("Set new rotation: 90")
@@ -157,14 +167,15 @@ def on_player_command(gametime:float, entity_type:str, entity_name:str, command:
             TurnableConveyorBelt.first().set_target_rotation(0)
             print("Set new rotation: 0")
             data.current_rotation = 0
-    if entity_name == "button2" and command == "Press":
+    if entity_name == "discard_button" and command == "Press":
         if MovablePlatform.first().get_current_rotation()[0] < 5:
             MovablePlatform.first().set_target_location(0, 0.7, 1.5)
             MovablePlatform.first().set_target_rotation(45.0, 0.0, 0.0)
         else: 
             MovablePlatform.first().set_target_location(0, 0, 0)
             MovablePlatform.first().set_target_rotation(0.0, 0.0, 0.0)
-        
+    
+
         
 editor.on_player_command(on_player_command)
 ### END ON PLAYER COMMAND CODE ###
@@ -172,13 +183,11 @@ editor.on_player_command(on_player_command)
 ### LEVEL TICK CODE - Add code that should be executed on every simulation tick. ###
 def on_tick(simtime: float, deltatime: float):
     spawn_next()
-    if ToggleSwitch.first().get_is_switched_on():
-        TurnableConveyorBelt.first().set_target_speed(4)
-    else: TurnableConveyorBelt.first().set_target_speed(0)
-
+    
     if data.box_count > 3:
         editor.set_goal_state("deliver_products",GoalState.Fail,"Failed: You jammed the delivery line!")
 
+    
 editor.on_tick(on_tick)
 ### END LEVEL TICK CODE ###
     
