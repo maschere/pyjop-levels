6859a2620ff791026113163b8fba6f52030b7d74
165274183+jalapih@users.noreply.github.com <165274183+jalapih@users.noreply.github.com>
2024-07-15 09:49:09 +0300

    add incomplete level crane ops

---
+++ src/logistics/Crane_Ops.py
@@ -0,0 +1,158 @@
+### INIT CODE - DO NOT CHANGE ###
+from pyjop import *
+
+SimEnv.connect()
+editor = LevelEditor.first()
+### END INIT CODE ###
+
+### IMPORTS - Add your imports here ###
+import random
+
+### END IMPORTS ###
+
+### DATA MODEL - Define a data model needed for this custom level to share data between function calls ###
+class DataModel(DataModelBase):
+    def __init__(self) -> None:
+        super().__init__()
+        self.correct_instructions = "UUUUURRRRRDDDDDLLLLL"
+        
+
+data = DataModel()
+### END DATA MODEL ###
+
+
+### CONSTRUCTION CODE - Add all code to setup the level (select map, spawn entities) here ###
+editor.select_map(SpawnableMaps.GrasslandOutdoor)
+editor.spawn_entity(SpawnableEntities.Killzone, "drop_zone", location=(0, 0, 0 ))
+editor.spawn_entity(SpawnableEntities.AirliftCrane, "crane", location=(0, 0, 5))
+
+editor.spawn_entity(SpawnableEntities.DataExchange, "control_center", location=(5, 3, 0))
+
+
+def spawn_temp_object():
+    editor.spawn_static_mesh(SpawnableMeshes.TireWheel, "tire", location=(5, -5, 1), rotation=(90, 0, 0), simulate_physics=True, is_temp=True)
+
+    
+def generate_instruction_sets(cargo_x, cargo_y, max_instructions=25): # cargo_x and cargo_y for possible generation of correct paths
+    
+    # This could also generate a random correct path, if I only knew how to do it properly
+    def select_correct_path():
+        data.correct_instructions = random.choice([
+        
+        ])
+
+    def generate_incorrect_path():
+        path = []
+        for _ in range(max_instructions):
+            path.append(random.choice(['L', 'R', 'U', 'D']))
+        return path
+
+
+    # Generate nine incorrect paths
+    instruction_sets = []
+    instruction_sets.append(data.correct_instructions)
+    while len(instruction_sets) < 10:
+        instruction_sets.append(generate_incorrect_path())
+
+    # Shuffle the instruction sets 
+    random.shuffle(instruction_sets)
+
+    return instruction_sets
+
+
+
+### END CONSTRUCTION CODE ###
+
+
+### GOAL CODE - Define all goals for the player here and implement the goal update functions. ###
+
+editor.set_goals_intro_text("There are crane instruction sets in the data exchange. Select the one that returns the crane to the "
+                           " drop zone and move the crane according to the instructions ")
+
+def sample_goal(goal_name: str):
+    editor.set_goal_progress(
+        goal_name,
+        1 / 30,
+        f"Follow the crane instructions that return the cargo to the drop zone",
+    )
+
+editor.specify_goal("levelGoal", "Follow the crane instructions that return the cargo to the drop zone", sample_goal)
+### END GOAL CODE ###
+
+
+### HINTS CHAT - Define custom hints as question / answer pairs that the player can get answers to via the assistant chat in-game. Consecutive hints are hidden until the direct precursor is revealed.###
+
+### END HINTS ###
+
+
+### ON BEGIN PLAY CODE - Add any code that should be executed after constructing the level once. ###
+def begin_play():
+    print("begin play")
+    AirliftCrane.first().editor_set_shake_intensity(0)
+    on_reset()
+
+
+editor.on_begin_play(begin_play)
+### END ON BEGIN PLAY CODE ###
+
+
+### ON LEVEL RESET CODE - Add code that should be executed on every level reset. ###
+def on_reset():
+    print("level resetting")
+    data.reset()
+    Killzone.first().set_autokill_enabled=True
+    spawn_temp_object()
+
+    # setup cargo location and generate crane instruction sets to data exchange    
+    cargo_x, cargo_y = 5, 5
+    instruction_sets = generate_instruction_sets(cargo_x, cargo_y)
+
+    for i, instructions in enumerate(instruction_sets):
+        DataExchange.first().set_data(f"Instruction set {i+1}",''.join(instructions) )
+    
+    
+
+editor.on_level_reset(on_reset)
+### END ON LEVEL RESET CODE ###
+
+
+### ON PLAYER COMMAND CODE - Add code that should be executed each time the player issues a code command to an entity
+def on_player_command(gametime:float, entity_type:str, entity_name:str, command:str, val:NPArray):
+    print(f"player command at {gametime}: {entity_type}.{entity_name}.{command}")
+
+editor.on_player_command(on_player_command)
+### END ON PLAYER COMMAND CODE ###
+
+### LEVEL TICK CODE - Add code that should be executed on every simulation tick. ###
+def on_tick(simtime: float, deltatime: float):
+    # Airlift Crane doesn't have built-in getter for location. Lets send it to data exchange but round to closest integer
+    crane_location = list(editor.get_location("crane"))
+    rounded_location = [round(val, 0) for val in crane_location]
+    
+    print("crane_location: ", rounded_location)
+    DataExchange.first().set_data("crane_location", rounded_location)
+    sleep(0.3)
+
+
+editor.on_tick(on_tick)
+### END LEVEL TICK CODE ###
+
+
+# set editor template code
+editor.set_template_code(from_comment="### PLAYER TEMPLATE CODE ###")
+
+
+### EOF CODE - DO NOT CHANGE ###
+editor.run_editor_level()
+### EOF ###
+
+
+### PLAYER TEMPLATE CODE ###
+#this template code will be provided to the player
+#add level specific hints or boilerplate code here.
+
+
+env = SimEnvManager.first()
+
+while SimEnv.run_main():
+    pass
